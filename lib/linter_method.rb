1dfc7c597c82fc938b4568b8f78c63da62ea559c
marysol <marilirulita@gmail.com>
2021-03-02 05:35:23 -0800

    Remove practice class

--- lib/linter_method.rb
+++ lib/linter_method.rb
@@ -1,16 +1,3 @@
-class SumofTwo
-  attr_accessor :numa, :numb
-
-  def initialize(numa, numb)
-    @numa = numa
-    @numb = numb
-  end
-
-  def adittion
-    numa + numb
-  end
-end
-
 class TestFile
   attr_accessor :file_data
 
