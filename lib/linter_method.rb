2c8de5720073ad49de564e5173d10441ffef9159
marysol <marilirulita@gmail.com>
2021-03-10 01:47:21 -0800

    Add call methods

--- lib/linter_method.rb
+++ lib/linter_method.rb
@@ -89,6 +89,7 @@ end
 
 some = TestFile.new('lib/tester.rb')
 some.check_indent
-# some.check_end
-# some.check_bracket
-# some.check_spaces
+some.check_end
+some.check_bracket
+some.check_end_spaces
+some.check_doub_spaces
