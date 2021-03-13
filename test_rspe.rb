6850f156b8280dc0ffef62ba651e5575e3356415
marysol <marilirulita@gmail.com>
2021-03-13 05:18:22 -0800

    Delete rspec files

--- test_rspe.rb
+++ test_rspe.rb
@@ -1,3 +1,34 @@
 def test.method
   2 + 2
 end
+
+
+require_relative '../lib/linter_method'
+require_relative './bin/main'
+require_relative './test_rspe'
+
+describe TestFile do
+  let(:str) { 'def  test_method(arg) {this is a block ' }
+  let(:options) { [1] }
+
+  describe '#initial_block' do
+    it 'return true if a string match any array element' do
+      test = TestFile.new('./bin/main.rb')
+      expect(test.initial_block(str)).to include('Dany')
+    end
+  end
+end
+
+describe Prints do
+  describe '#prints_puts' do
+    it 'return true if a string match any array element' do
+      expect(prints_put).to include('Dany')
+    end
+  end
+end
+
+describe '#test.method' do
+  it 'returns 4' do
+    expect(test_method).to eql(4)
+  end
+end
