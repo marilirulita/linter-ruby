0a89b37b6f7db8a4835fa8c3b907f8b47e8d8097
marysol <marilirulita@gmail.com>
2021-03-14 09:09:58 -0700

    Add rspec files

--- test_rspe.rb
+++ test_rspe.rb
@@ -1,43 +1,42 @@
-def test_method
-  2 + 2
-end
-
-=begin
-  
-require_relative '../lib/linter_method'
-require_relative './bin/main'
-require_relative './test_rspe'
-
-describe TestFile do
-  let(:str) { 'def  test_method(arg) {this is a block ' }
-  let(:options) { [1] }
-
-  describe '#initial_block' do
-    it 'return true if a string match any array element' do
-      test = TestFile.new('./bin/main.rb')
-      expect(test.initial_block(str)).to include('Dany')
-    end
+class Calculator
+  def add(a,b)
   end
 end
 
-describe Prints do
-  describe '#prints_puts' do
-    it 'return true if a string match any array element' do
-      expect(prints_put).to include('Dany')
-    end
-  end
-end
-
-
-require_relative './test_rspe'
-
-def test_method
-  2 + 2
-end
-
-describe "#test_method" do
-  it "returns 4" do
-    expect(5+3).to eql(8)
-  end
-end
-=end
+#
+# require_relative '../lib/linter_method'
+# require_relative './bin/main'
+# require_relative './test_rspe'
+#
+# describe TestFile do
+#   let(:str) { 'def  test_method(arg) {this is a block ' }
+#   let(:options) { [1] }
+#
+#   describe '#initial_block' do
+#     it 'return true if a string match any array element' do
+#       test = TestFile.new('./bin/main.rb')
+#       expect(test.initial_block(str)).to include('Dany')
+#     end
+#   end
+# end
+#
+# describe Prints do
+#   describe '#prints_puts' do
+#     it 'return true if a string match any array element' do
+#       expect(prints_put).to include('Dany')
+#     end
+#   end
+# end
+#
+#
+# require_relative './test_rspe'
+#
+# def test_method
+#   2 + 2
+# end
+#
+# describe "#test_method" do
+#   it "returns 4" do
+#     expect(5+3).to eql(8)
+#   end
+# end
